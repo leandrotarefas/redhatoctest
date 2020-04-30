b9f142285e05e2dab72a7b13799d7453a9c3d318
leandrotarefas <conta.tarefas@gmail.com>
2020-04-30 02:47:21 -0300

    add express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("teste!")
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello world!\n');
+  });
+
+app.get('/mars', function(req, res) {
+    res.send('Hello Mars!\n');
+  });
+
+  app.listen(8080,function() {
+    console.log("app inicializada co sucesso!");
+  })
